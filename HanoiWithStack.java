dea3631cf4f497b781d1c41d2cf08cd93dd74793
peiying yan <ypypatricka@gmail.com>
2014-07-04 15:04:22 -0400

    Create HanoiWithStack.java

---
+++ HanoiWithStack.java
@@ -0,0 +1,22 @@
+public class HanoiWithStack {
+	public void hanoi(int n, char src, char bri, char dst){
+		Stack<st> stack = new Stack<st>();
+		st temp;
+		stack.push(new st(1,n,src,bri,dst));
+		while(!stack.isEmpty()){
+			temp = stack.pop();
+			if(temp.begin != temp.end){
+				stack.push(new st(temp.begin, temp.end-1, temp.bri, temp.src, temp.dst));
+				stack.push(new st(temp.end, temp.end, temp.src, temp.bri, temp.dst));
+				stack.push(new st(temp.begin, temp.end-1, temp.src, temp.dst, temp.bri));
+			}else{
+				System.out.println("Move Dist" + temp.begin + " from " + temp.src + " to " + temp.dst);
+			}
+		}
+	}
+    public static void main(String[] args) {
+    	
+    	HanoiWithStack s1 = new Solution();
+    	s1.hanoi(3, 'A', 'B', 'C');
+	}
+}
