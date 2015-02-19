061983ea210c2e64ae35b0f7c79cf670be0f7adc
peiying yan <ypypatricka@gmail.com>
2015-02-19 14:53:46 -0500

    Create Solution.py

---
+++ leetcod_python/Subsets-II/Solution.py
@@ -0,0 +1,15 @@
+# dfs版本 只要加一个条件
+class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integer
+    def subsetsWithDup(self, S):
+        def dfs(depth, start, valuelist):
+            if valuelist not in res: res.append(valuelist)
+            if depth == len(S): return
+            for i in range(start, len(S)):
+                dfs(depth+1, i+1, valuelist+[S[i]])
+        S.sort()
+        res = []
+        dfs(0, 0, [])
+        return res
+        
