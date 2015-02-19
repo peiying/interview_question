6af833bb73da6d8fa953802d42af1433f268f433
peiying yan <ypypatricka@gmail.com>
2015-02-19 14:35:27 -0500

    Update Solution.py

--- leetcod_python/Subsets/Solution.py
+++ leetcod_python/Subsets/Solution.py
@@ -1,15 +1,12 @@
+#搞了很久，还是可以用java的iterator思路
+#可以用 + 来进行深copy
 class Solution:
     # @param S, a list of integer
     # @return a list of lists of integer
   def subsets(self, S):
-        def dfs(depth, start, valuelist):
-            res.append(valuelist)
-            if depth == len(S): return
-            for i in range(start, len(S)):
-                dfs(depth+1, i+1, valuelist+[S[i]])
-        S.sort()
-        res = []
-        dfs(0, 0, [])
-        return res
-            
-        
+      S.sort()
+      res = [[]]
+      for i in range(len(S)):
+          for j in range(len(res)):
+              res.append(res[j] + [S[i]])
+      return res
