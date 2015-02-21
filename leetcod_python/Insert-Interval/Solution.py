51b1e0e34e0dea6b4d69307885ae88c0a91ce367
peiying yan <ypypatricka@gmail.com>
2015-02-21 13:24:43 -0500

    Update and rename leetcod_python to leetcod_python/Insert-Interval/Solution.py

---
+++ leetcod_python/Insert-Interval/Solution.py
@@ -0,0 +1,28 @@
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    # @param intervals, a list of Intervals
+    # @param newInterval, a Interval
+    # @return a list of Interval
+    def insert(self, intervals, newInterval):
+        intervals.append(newInterval)
+        if not intervals or len(intervals) == 1:
+            return intervals
+        intervals.sort(key=lambda x: x.start)
+        pre = intervals[0]
+        res = []
+        curIdx = 0
+        res.append(intervals[0])
+        for interval in intervals[1:]:
+            if interval.start <= pre.end:
+                right = max(pre.end, interval.end)
+                res[curIdx].end = right
+            else:
+                res.append(interval)
+                pre = interval
+                curIdx += 1
+        return res
