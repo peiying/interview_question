dc0f17f8d9cd1fba7d7293925c1dcab5c4579efb
peiying yan <ypypatricka@gmail.com>
2015-03-15 00:12:27 -0400

    Create Solution.java

---
+++ tripadvisor/demo/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] A) {
+        int n = A.length;
+        if (n==0) return -1; 
+        long sum = 0; 
+        for(int i = 0; i < n; i++) sum += (long) A[i]; 
+        long sum_left = 0;    
+        for(int i = 0; i < n; i++) {
+            long sum_right = sum - sum_left - (long) A[i];
+            if (sum_left == sum_right) return i;
+            sum_left += (long) A[i];
+        } 
+        return -1; 
+    }
+}
